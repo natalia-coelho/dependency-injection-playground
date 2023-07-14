26a0fd8e269e8b6e5ab5cc64a382f33f21c869c1
natalia-coelho <nataliacoelho199@gmail.com>
2023-07-13 23:19:09 -0300

    Initial Files

---
+++ Ninject/NinjectDemo/Samurai.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectDemo
+{
+    class Samurai
+    {
+        readonly Sword sword;
+
+        public Samurai()
+        {
+            this.sword = new Sword();
+        }
+
+        public void Attack(string target)
+        {
+            this.sword.Hit(target);
+        }
+    }
+}
---
+++ Ninject/NinjectDemo/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectDemo
+{
+        class Sword
+        {
+            public void Hit(string target)
+            {
+                Console.WriteLine($"Chopped {target} clean in half.");
+            }
+        }
+}
